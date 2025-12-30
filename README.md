20514d1cec7c81b76a95eeb05e1151f6f0fab8d5
unknown <bryan1736b@gmail.com>
2025-12-30 08:02:44 -0500

    initial

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 # Elysia with Bun runtime
 
 ## Getting Started
+
 To get started with this template, simply paste this command into your terminal:
+
 ```bash
 bun create elysia ./elysia-example
 ```
 
 ## Development
+
 To start the development server run:
+
 ```bash
 bun run dev
 ```
 
 Open http://localhost:3000/ with your browser to see the result.
+
+## Railway
+
+### Opción 1: usando Docker (recomendado)
+
+```dockerfile
+RUN chmod +x binLinux/rhubarb
+```
+
+### Opción 2: sin Docker (Start Command)
+
+```bash
+chmod +x binLinux/rhubarb && bun run start
+```
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,28 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
+const RHUBARD =
+  process.env.NODE_ENV === "production"
+    ? "./binLinux/rhubarb"
+    : "./binWindows/rhubarb.exe";
+
+app.get("generate", async () => {
+  const process = Bun.spawn([
+    RHUBARD,
+    "./uploads/defeated.wav",
+    "-f",
+    "json",
+    "-o",
+    "./output/defeated.json",
+  ]);
+
+  await process.exited;
+
+  const json = await Bun.file("./output/defeated.json").json();
+
+  return json;
+});
+
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
